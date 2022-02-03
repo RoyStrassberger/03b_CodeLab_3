67f112558033ea968d68e70f09538eb8f738ab53
RoyStrassberger <rdf3001@googlemail.com>
2022-02-03 09:32:01 +0100

    Update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,8 +3,13 @@ package com.cc.java;
 public class App {
 
     public static void main(String[] args) {
-        Mittarbeiter mittarbeiter()  
-         = new.getMitarbeiterAtributes();
+        Mittarbeiter mittarbeiter = new.getMitarbeiterAtributes("Hofer","Karl",1971,007,"IT-Service","Aushilfe");
+        output("Nachname: " + Mittarbeiter.lastNameString());
+        output("Vorname: " + Mittarbeiter.firstNameString());
+        output("Geboren: " + Mittarbeiter.birthYear());
+        output("Arbeiter-Nr.: "+ Mittarbeiter.workID());
+        output("Abteilung: "+ Mittarbeiter.departmentString());
+        output("Arbeitsrolle: "+ Mittarbeiter.roleString());
 
       
     }
